5b5876618c31d1df75b2cc2649a1603ab7055475
TSdnl13 <daniel.int@outlook.com>
2022-05-30 22:21:50 -0500

    setup backend

---
+++ todolist-backend/src/test/java/com/example/todots/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
