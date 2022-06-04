2bfb6b0b667f448a001459fc5da60375629faa57
TSdnl13 <daniel.int@outlook.com>
2022-06-04 18:59:18 -0500

    add: User Sumary projection and ger usersummary in userRepository

---
+++ todolist-backend/src/main/java/com/example/todots/entity/projections/UserSummary.java
@@ -0,0 +1,8 @@
+package com.example.todots.entity.projections;
+
+public interface UserSummary {
+   public Integer getUserId();
+   public String getEmail();
+   public String getName();
+   public String getPassword();
+}
--- todolist-backend/src/main/java/com/example/todots/repository/UserRepository.java
+++ todolist-backend/src/main/java/com/example/todots/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.todots.repository;
 
 import com.example.todots.entity.User;
+import com.example.todots.entity.projections.UserSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
    Optional<User> findByEmail(String email);
+
+   @Query("select u from User u where u.email = ?1")
+   Optional<UserSummary> findUserSummaryByEmail(String email);
+
 }
