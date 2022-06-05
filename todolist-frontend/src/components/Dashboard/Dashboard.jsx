1e4496527e7cbdeb42363d7c8d8f12e69da615da
TSdnl13 <daniel.int@outlook.com>
2022-06-05 16:07:32 -0500

    add: get taskLists

--- todolist-frontend/src/components/Dashboard/Dashboard.jsx
+++ todolist-frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Navbar from '../Navbar/Navbar';
 import Sidebar from '../Sidebar/Sidebar';
 import Tasks from '../Tasks/Tasks';
 
 import './Dashboard.scss';
 
 const Dashboard = () => {
+   const user = JSON.parse(localStorage.getItem('user'));
+
+   const [taskLists, setTaskLists] = useState([]);
+   
+   
+   useEffect(() => {
+      const getUserTaskLists = async () => {
+         try {
+            const response = await axios.get(`http://localhost:8080/api/taskList/user?id=${user?.userId}`);
+            setTaskLists(response.data);
+            console.log(taskLists);
+         } catch (error) {
+            console.log(error);
+         }
+      }
+      
+      getUserTaskLists();
+   
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
+   
+   
+   
    return (
-      <div>
-         <Sidebar />
-         <Tasks />
+      <div className='dashboard'>
+         <Sidebar className='sidebar' taskLists={taskLists} setTaskLists={setTaskLists} />
+         <div className='dashboard__panel'>
+            <Navbar user={user} />
+            <Tasks />
+         </div>
       </div>
    )
 }
