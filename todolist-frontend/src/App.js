cb0165552f898330bac7aa9ab225d75b673b501f
TSdnl13 <daniel.int@outlook.com>
2022-06-04 23:54:39 -0500

    refactoring, change name from Home to Dashoard

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import './App.scss';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
    return (
@@ -11,7 +11,7 @@ const App = () => {
          <Routes>
             <Route path='/' element={<Navigate to='/auth' />} />
             <Route path='/auth' element={<Auth />} />
-            <Route path='/home' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
          </Routes>
       </Router>
    );
