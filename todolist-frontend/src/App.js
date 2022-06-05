400da388c9b27a795fd224204256cc21413164b2
TSdnl13 <daniel.int@outlook.com>
2022-06-04 21:12:27 -0500

    add: auth and home routes

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Auth from './components/Auth/Auth';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 import './App.scss';
 
 const App = () => {
    return (
-      <Auth />
+      <Router>
+         <Routes>
+            <Route path='/' element={<Navigate to='/auth' />} />
+            <Route path='/auth' element={<Auth />} />
+            <Route path='/home' element={<Home />} />
+         </Routes>
+      </Router>
    );
 }
 
