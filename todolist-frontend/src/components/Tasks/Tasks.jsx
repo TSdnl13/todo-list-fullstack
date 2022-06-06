6379b6ec05f89a9929841a455d333e4d881efbf3
TSdnl13 <daniel.int@outlook.com>
2022-06-05 23:06:44 -0500

    add code until show tasks of a task list

--- todolist-frontend/src/components/Tasks/Tasks.jsx
+++ todolist-frontend/src/components/Tasks/Tasks.jsx
@@ -2,9 +2,44 @@ import React from 'react';
 
 import './Tasks.scss';
 
-const Tasks = () => {
+
+const Tasks = ({ tasks }) => {
+   console.log(tasks);
+   const completedTasks = tasks.tasks?.filter(task => task.state === true);
+   const pendingTasks = tasks.tasks?.filter(task => task.state === false);
+
    return (
-      <div>Tasks</div>
+      <div className='tasks'>
+         <div>
+            <h2>{tasks?.taskListName}</h2>
+            {pendingTasks?.length > 0 && (
+               <ul>
+                  {pendingTasks.map(task => (
+                     <li key={task.taskId}>
+                        <p>{task.name}</p>
+                     </li>
+                  ))}
+               </ul>
+            )}
+            <ul>
+            </ul>
+            {completedTasks?.length > 0 && (
+            <div>
+               <h3>Completed {completedTasks.length}</h3>
+               <ul>
+                  {completedTasks.map(task => (
+                     <li key={task.taskId}>
+                        <p>{task.name}</p>
+                     </li>
+                  ))}
+               </ul>
+            </div>
+            )}
+
+         </div>
+
+         <input type='text' placeholder='New Task' />
+      </div>
    )
 }
 
