9e365d1990707d9d573e7b68d6f00b5a0b2095d9
TSdnl13 <daniel.int@outlook.com>
2022-06-06 23:06:19 -0500

    add DatePicker and height tasks container and its styles

--- todolist-frontend/src/components/Tasks/Tasks.jsx
+++ todolist-frontend/src/components/Tasks/Tasks.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddSharpIcon from '@mui/icons-material/AddSharp';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import './Tasks.scss';
-
+import { DueDateTextField } from '../Inputs/Inputs';
 
 const Tasks = ({ tasks }) => {
-   console.log(tasks);
+   const [value, setValue] = useState('');
    const completedTasks = tasks.tasks?.filter(task => task.state === true);
    const pendingTasks = tasks.tasks?.filter(task => task.state === false);
+   const navbar = document.getElementById('navbar');
+
+   const darkDatePicker = createTheme({
+      palette: {
+         mode: 'dark',
+         error: {main: '#292f58'}
+      }
+    });
 
    return (
-      <div className='tasks'>
-         <div>
+      <div className='tasks' style={{ height: `calc(100vh - ${navbar?.offsetHeight}px)`}}>
+         <div className='tasks__container'>
             <h2>{tasks?.taskListName}</h2>
             {pendingTasks?.length > 0 && (
                <ul>
@@ -21,11 +34,10 @@ const Tasks = ({ tasks }) => {
                   ))}
                </ul>
             )}
-            <ul>
-            </ul>
+
             {completedTasks?.length > 0 && (
             <div>
-               <h3>Completed {completedTasks.length}</h3>
+               <p className='tasks-subtile'>Completed <span>{completedTasks.length}</span></p>
                <ul>
                   {completedTasks.map(task => (
                      <li key={task.taskId}>
@@ -37,8 +49,24 @@ const Tasks = ({ tasks }) => {
             )}
 
          </div>
-
-         <input type='text' placeholder='New Task' />
+         <div className='tasks__create-task'>
+            <AddSharpIcon fontSize='small' />
+            <input type='text' placeholder='New Task' />
+            <ThemeProvider theme={darkDatePicker}>
+               <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                     value={value}
+                     onChange={(newValue) => {
+                        setValue(newValue);
+                     }}
+                     renderInput={(params) => (
+                        <DueDateTextField size='small' {...params} />
+                     )}
+                  />
+               </LocalizationProvider>
+            </ThemeProvider>
+            <button type='button'>Create</button>
+         </div>
       </div>
    )
 }
