b77974f4a158f0ec6c7842d682206dbf7f4f52aa
TSdnl13 <daniel.int@outlook.com>
2022-06-19 11:09:51 -0500

    deleted redirect if no user-not working

--- todolist-frontend/src/components/Dashboard/Dashboard.jsx
+++ todolist-frontend/src/components/Dashboard/Dashboard.jsx
@@ -10,7 +10,6 @@ import './Dashboard.scss';
 const Dashboard = () => {
    const user = JSON.parse(localStorage.getItem('user'));
 
-   const navigate = useNavigate();
    const [taskLists, setTaskLists] = useState([]);
    const [taskListId, setTaskListId] = useState(0);
    const [showSidebar, setShowSidebar] = useState(false);
@@ -18,10 +17,6 @@ const Dashboard = () => {
    const [tasks, setTasks] = useState({taskListName: '', tasks: []});
 
    useEffect(() => {
-      if (!user ) {
-         navigate('/auth');
-         return;
-      }
       axios.get(`https://spring-tstodolist.herokuapp.com/api/task/important/${user?.userId}`)
       .then(response => {
          setTasks({ taskListName: 'Important', tasks: response.data });
@@ -29,15 +24,10 @@ const Dashboard = () => {
       .catch(error => {
          console.log(error);
       });
-   // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [user?.userId]);
    
 
    useEffect(() => {
-      if (!user ) {
-         navigate('/auth');
-         return;
-      }
       axios.get(`https://spring-tstodolist.herokuapp.com/api/taskList/user?id=${user?.userId}`)
          .then((response) => {
             setTaskLists(response.data);
